0430ca69ed47e4dcca4414e8d7685e59ebf82f84
Oscar Carrasco <anubis.carrasco03@gmail.com>
2019-10-27 23:35:44 -0600

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Menu from "./components/navs/Menu";
-import Bio from './components/home/index';
+import Bio from "./components/home/index";
 import About from "./components/about/index";
 import { CssBaseline, Container, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(4)
   },
   mt5: {
-    marginTop: theme.spacing(5),
+    marginTop: theme.spacing(5)
   }
 }));
 
@@ -17,14 +18,16 @@ function App() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg" className={classes.root}>
-        <Menu />
-        <Bio className={classes.mt5} />
-        {/* <About /> */}
-      </Container>
-    </React.Fragment>
+    <BrowserRouter>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg" className={classes.root}>
+          <Menu />
+          <Route exact path='/' component={ Bio } className={classes.mt5} />
+          <Route exact path='/about' component={ About }  />
+        </Container>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
